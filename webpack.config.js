560587a72a2abf4c3e61a5223ae1fc9b74be5966
Stanislav <stanislav.khomyn@skylum.com>
2020-06-08 13:09:57 -0700

    mini css extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -43,13 +44,13 @@ module.exports = {
       // Loading CSS
       {
         test: /\.(css)$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
       },
 
       // Loading SASS/SCSS
       {
         test: /\.(s[ca]ss)$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
       }
 
     ]
@@ -60,6 +61,9 @@ module.exports = {
       title: 'Hello World',
       buildTime: new Date().toISOString(),
       template: 'public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-[hash:8].css'
     })
   ]
 };
